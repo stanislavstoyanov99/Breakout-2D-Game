ce92b8dc90ad18f027b41a5d48701a590ae3c730
Stanislav Stoyanov <slavkata_99@abv.bg>
2021-06-06 17:13:41 +0300

    Add vertex and index buffers into separate folder
    -- Add vertex array, which uses them
    #2

--- src/Breakout/Application.h
+++ src/Breakout/Application.h
@@ -2,6 +2,8 @@
 
 #include <glm/glm.hpp>
 
+#include "VertexArray.h"
+
 struct GLFWwindow;
 
 class Application
@@ -19,7 +21,8 @@ private:
 	friend void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 	friend void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 
-private:
+	std::unique_ptr<VertexArray> _va;
+
 	glm::vec3 _lightPos = glm::vec3(1.2f, 1.0f, 2.0f);
 	glm::vec3 _lightColor = glm::vec3(1.f, 1.f, 1.f);
 
